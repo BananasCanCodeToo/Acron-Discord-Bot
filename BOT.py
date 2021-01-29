c04dbb0ff3a3ce79691fb172430e866e069ac021
BananasCanCodeToo <66839366+BananasCanCodeToo@users.noreply.github.com>
2021-01-29 18:52:23 -0500

    Reaction Game Update

--- BOT.py
+++ BOT.py
@@ -13,9 +13,124 @@ from datetime import timedelta
 # Registering the bot with a prefix
 bot = commands.Bot(command_prefix = '!')
 
+@bot.event
+async def on_message(message):
+    x = datetime.datetime.now()
+    msg = f"{message.content}"
+    msglen = len(msg)
+    botID = bot.user.id
+    if message.author.id != botID:
+        if msg[0] != '!':
+            num = False
+            code = ""
+            attempt = 0
+            for i in range(msglen):
+                cde = ""
+                if attempt > 0:
+                    attempt += -1
+                else:
+                    try:
+                        intTestI = int(msg[i])
+                        for e in msg[i:]:
+                            try:
+                                intI = int(e)
+                                cde += e
+                            except:
+                                if e == " ":
+                                    continue
+                                elif e != " ":
+                                    num = False
+                                    cde = ""
+                            if len(cde) == 6:
+                                code = cde
+                                num = True
+                                break
+                    except:
+                        if msg[i-1] + msg[i] == "<@":
+                            attempt = 18
+                        continue
+                    if len(cde) == 6:
+                        code = cde
+                        num = True
+                        break
+
+            if num == True:
+                print(f'[INFO] {datetime.datetime.now().strftime("%H:%M:%S")} > {message.author.name}#{message.author.discriminator} sent a raw code ({code})')
+                with open('log.txt','a') as f:
+                    f.write(f'\n[INFO] {x.strftime("%H:%M:%S")} > {message.author.name}#{message.author.discriminator} sent a raw code ({code})')
+                await message.add_reaction(emoji="💬")
+
+                with open('pgames.data','r') as f:
+                    messages = json.load(f)
+                messages[f'{message.author.id}'] = [message.id,f'{code}']
+                with open('pgames.data','w+') as f:
+                    json.dump(messages, f, indent=4)
+
+        else:
+            await bot.process_commands(message)
+@bot.event
+async def on_raw_reaction_add(payload):
+    x = datetime.datetime.now()
+    botID = bot.user.id
+    with open('pgames.data','r') as f:
+        messageIDs = json.load(f)
+    for i,e in messageIDs.items():
+        if e[0] == payload.message_id:
+            if payload.member.id == int(i):
+                if payload.member.id != botID:
+                    with open('games.data','r') as f:
+                        games = json.load(f)
+                    game = False
+                    # looks to see if this player already has a game
+                    for i,e in games.items():
+                        if i == f'{payload.member.id}':
+                            game = True
+                            break
+
+                    if game == False:
+                        user = payload.member
+                        code = e[1]
+                        with open('SETTINGS.txt','r') as f:
+                            settings = yaml.load(f,Loader=yaml.FullLoader)
+                            gChannel = settings['GAME_CHANNEL']
+                        gameChannel = bot.get_channel(gChannel)
+                        currentPing = x
+                        ping = ''
+                        with open('ping.data','rb') as f:
+                            pastPing = pickle.load(f)
+                        with open('SETTINGS.txt','r') as f:
+                            settings = yaml.load(f,Loader=yaml.FullLoader)
+                            timeout = settings['PING_TIMEOUT']
+                        if currentPing > pastPing + timedelta(minutes=timeout):
+                            with open('ping.data','wb') as f:
+                                pickle.dump(currentPing, f)
+                                ping = bot.get_guild(payload.guild_id).get_role(settings['PING_ROLE']).mention
+                        # Sends game code
+                        e = discord.Embed(colour=discord.Color.orange(),title=f"**{code}**")
+                        e.set_footer(text=f'{payload.member.name}#{payload.member.discriminator}',icon_url=f'{payload.member.avatar_url}')
+                        message = await gameChannel.send(embed = e, content = f'{ping}')
+                        await payload.member.send(f'Successfully started the game. Remember to use `!endgame` when you are done!')
+                        print(f'[INFO] {x.strftime("%H:%M:%S")} > {payload.member.name}#{payload.member.discriminator} started game from reaction.')
+                        with open('log.txt','a') as f:
+                            f.write(f'\n[INFO] {x.strftime("%H:%M:%S")} > {payload.member.name}#{payload.member.discriminator} started game from reaction.')
+                        with open('pgames.data','r') as f:
+                            messages = json.load(f)
+                        messages.pop(f'{payload.member.id}')
+                        with open('pgames.data','w+') as f:
+                            json.dump(messages, f, indent=4)
+                        with open('games.data','r') as f:
+                            games = json.load(f)
+                        # Writes the game to the Game JSON file
+                        with open('games.data','w') as f:
+                            games[f'{payload.member.id}'] = [message.id,code]
+                            json.dump(games, f, indent=4)
+                    else:
+                        await payload.member.send(f'{payload.member.mention}, You already have a game running. Use `!endgame` to mark your previous game as ended.\n*Un-react to the message then react again after you end your previous game.*')
+
+
 # The command to start games
 @bot.command()
-async def game(ctx, c1, c2, c3):
+async def game(ctx, *, code):
     # Finds the date and finds the specified game channel
     x = datetime.datetime.now()
     with open('SETTINGS.txt','r') as f:
@@ -56,20 +171,21 @@ async def game(ctx, c1, c2, c3):
                 ping = ctx.guild.get_role(settings['PING_ROLE']).mention
 
         # Sends game code
-        e = discord.Embed(colour=discord.Color.orange(),title=f"**{c1} {c2} {c3}**")
+        e = discord.Embed(colour=discord.Color.orange(),title=f"**{code}**")
         e.set_footer(text=f'{ctx.author.name}#{ctx.author.discriminator}',icon_url=f'{ctx.author.avatar_url}')
         message = await gameChannel.send(embed = e, content = f'{ping}')
 
         await ctx.author.send(f'Successfully started the game. Remember to use `!endgame` when you are done!')
 
-        # Logs the successful start of a game
-        print(f'[INFO] {x.strftime("%H:%M:%S")} > {ctx.author.name}#{ctx.author.discriminator} Successfully started Game. Code: {c1} {c2} {c3}')
+        print(f'[INFO] {x.strftime("%H:%M:%S")} > {ctx.author.name}#{ctx.author.discriminator} Successfully started Game. Code: {code}')
         with open('log.txt','a') as f:
-            f.write(f'\n[INFO] {x.strftime("%H:%M:%S")} > {ctx.author.name}#{ctx.author.discriminator} Successfully started Game. Code: {c1} {c2} {c3}')
+            f.write(f'\n[INFO] {x.strftime("%H:%M:%S")} > {ctx.author.name}#{ctx.author.discriminator} Successfully started Game. Code: {code}')
 
+        with open('games.data','r') as f:
+            games = json.load(f)
         # Writes the game to the Game JSON file
         with open('games.data','w') as f:
-            games[f'{ctx.author.id}'] = [message.id,c1,c2,c3]
+            games[f'{ctx.author.id}'] = [message.id,code]
             json.dump(games, f, indent=4)
     else:
         await ctx.channel.purge(limit=1)
@@ -93,9 +209,7 @@ async def endgame(ctx):
     if game == True:
         contents = games[f'{ctx.author.id}']
         messageID = contents[0]
-        c1 = contents[1]
-        c2 = contents[2]
-        c3 = contents[3]
+        code = contents[1]
 
         # Removes game from games.json
         games.pop(f'{ctx.author.id}')
@@ -110,8 +224,9 @@ async def endgame(ctx):
         message = await channel.fetch_message(int(messageID))
 
         # Edits the embed to say the game has ended
-        e = discord.Embed(colour=discord.Color.red(),title=f"**{c1} {c2} {c3}** *ENDED*")
+        e = discord.Embed(colour=discord.Color.red(),title="*ENDED*",description=f"**{code}**")
         e.set_footer(text=f'{ctx.author.name}#{ctx.author.discriminator}',icon_url=f'{ctx.author.avatar_url}')
+
         await message.edit(embed = e, content = '')
         await ctx.author.send('Your Game has been marked as ended.')
 
@@ -119,10 +234,8 @@ async def endgame(ctx):
         print(f'[INFO] {x.strftime("%H:%M:%S")} > {ctx.author.name}#{ctx.author.discriminator} Has ended a game.')
         with open('log.txt','a') as f:
             f.write(f'\n[INFO] {x.strftime("%H:%M:%S")} > {ctx.author.name}#{ctx.author.discriminator} Has ended a game.')
-
-
-
-
+    else:
+        await ctx.author.send("You don't have a game running!")
 
 
 
@@ -135,6 +248,9 @@ async def on_ready():
     print(f'[INFO] {x.strftime("%H:%M:%S")} > Bot is Online')
     with open('log.txt','a') as f:
         f.write(f'\n\n\n\n[DATE] {x.strftime("%Y.%m.%d")}\n[INFO] {x.strftime("%H:%M:%S")} > Bot is Online')
+
+
+
 
 
 # BOT TOKEN TO RUN!!!! DO NOT SHARE!!!!
